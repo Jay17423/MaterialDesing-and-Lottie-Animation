6ef97ec95df6e0978cc56f4adad77f70979e73ba
jay17423 <jayprakash1200@gmail.com>
2024-01-28 20:39:18 +0530

    Material Design and Lottie Animation

---
+++ app/src/main/java/com/maverickallies/lottieanimation/First_activity.java
@@ -0,0 +1,14 @@
+package com.maverickallies.lottieanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class First_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
